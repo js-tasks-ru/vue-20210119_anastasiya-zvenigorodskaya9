0cd0a6f3724859d76f1a8ff4481d28c1d6d478bd
zvenigorodskaia <work1908@gmail.com>
2021-01-23 21:10:56 +0300

    add solution for 01 task: Click button

--- 01-basics/01-CounterButton/script.js
+++ 01-basics/01-CounterButton/script.js
@@ -2,3 +2,16 @@ import Vue from './vue.esm.browser.js';
 
 // const app = ...
 // Рекомендуется использовать МЕТОД в качестве обработчика события
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'click on me!',
+    value: 0,
+  },
+  methods: {
+    onBtnClick() {
+      return this.value++;
+    },
+  },
+});
