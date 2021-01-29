e6a246907c61984f8f65d5d170fd8599fd14016e
zvenigorodskaia <work1908@gmail.com>
2021-01-29 11:38:04 +0300

    02-MeetupPageDraft: fix rawMeetup

--- 01-basics/02-MeetupPageDraft/script.js
+++ 01-basics/02-MeetupPageDraft/script.js
@@ -3,9 +3,6 @@ import Vue from './vue.esm.browser.js';
 /** URL адрес API */
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-const fetchMeetups = () =>
-  fetch(`${API_URL}/meetups`).then((res) => res.json());
-
 const MEETUP_ID = 6;
 const fetchMeetup = () =>
   fetch(`${API_URL}/meetups/${MEETUP_ID}`).then((res) => res.json());
@@ -22,78 +19,74 @@ const getDateTimeString = (date) => {
 /**
  * Словарь заголовков по умолчанию для всех типов элементов программы
  */
-// const agendaItemTitles = {
-//   registration: 'Регистрация',
-//   opening: 'Открытие',
-//   break: 'Перерыв',
-//   coffee: 'Coffee Break',
-//   closing: 'Закрытие',
-//   afterparty: 'Afterparty',
-//   talk: 'Доклад',
-//   other: 'Другое',
-// };
+const agendaItemTitles = {
+  registration: 'Регистрация',
+  opening: 'Открытие',
+  break: 'Перерыв',
+  coffee: 'Coffee Break',
+  closing: 'Закрытие',
+  afterparty: 'Afterparty',
+  talk: 'Доклад',
+  other: 'Другое',
+};
 
 /**
  * Словарь иконок для для всех типов элементов программы.
  * Соответствует имени иконок в директории /assets/icons
  */
-// const agendaItemIcons = {
-//   registration: 'key',
-//   opening: 'cal-sm',
-//   talk: 'tv',
-//   break: 'clock',
-//   coffee: 'coffee',
-//   closing: 'key',
-//   afterparty: 'cal-sm',
-//   other: 'cal-sm',
-// };
+const agendaItemIcons = {
+  registration: 'key',
+  opening: 'cal-sm',
+  talk: 'tv',
+  break: 'clock',
+  coffee: 'coffee',
+  closing: 'key',
+  afterparty: 'cal-sm',
+  other: 'cal-sm',
+};
 
 export const app = new Vue({
   el: '#app',
 
   data() {
     return {
-      rawMeetups: [],
-      rawMeetup: [],
-      agenda: [],
+      rawMeetup: null,
     };
   },
 
   async mounted() {
     // Требуется получить данные митапа с API
-    this.rawMeetups = await fetchMeetups();
     this.rawMeetup = await fetchMeetup();
-    this.agenda = this.rawMeetup.agenda;
   },
-
   computed: {
-    meetups() {
-      return this.getMeetupsData();
+    meetup() {
+      return this.rawMeetup;
     },
   },
 
   methods: {
-    // Получение данных с API предпочтительнее оформить отдельным методом,
-    // а не писать прямо в mounted()
-    getMeetupsData() {
-      return this.rawMeetups.map((meetup) => ({
-        ...meetup,
-        cover: meetup.imageId
-          ? `https://course-vue.javascript.ru/api/images/${meetup.imageId}`
-          : undefined,
-        coverStyle: meetup.imageId
-          ? {
-              '--bg-url': `url('https://course-vue.javascript.ru/api/images/${meetup.imageId}')`,
-            }
-          : {},
-        date: new Date(meetup.date),
-        localDate: new Date(meetup.date).toLocaleString(navigator.language, {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        }),
-        getDateTimeString: getDateTimeString(new Date(meetup.date)),
-      }));
+    makeMeetupCoverLink(imageId) {
+      return `url(${API_URL}/images/${imageId})`;
     },
+    // getMeetupData() {
+    //   return this.rawMeetup.map((meetup) => ({
+    //     ...meetup,
+    //     cover: meetup.imageId
+    //       ? `https://course-vue.javascript.ru/api/images/${meetup.imageId}`
+    //       : undefined,
+    //     coverStyle: meetup.imageId
+    //       ? {
+    //           '--bg-url': `url('https://course-vue.javascript.ru/api/images/${meetup.imageId}')`,
+    //         }
+    //       : {},
+    //     date: new Date(meetup.date),
+    //     localDate: new Date(meetup.date).toLocaleString(navigator.language, {
+    //       year: 'numeric',
+    //       month: 'long',
+    //       day: 'numeric',
+    //     }),
+    //     getDateTimeString: getDateTimeString(new Date(meetup.date)),
+    //   }));
+    // },
   },
 });
