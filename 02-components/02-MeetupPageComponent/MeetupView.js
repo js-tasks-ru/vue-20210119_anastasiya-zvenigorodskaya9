edb4c1e573e979c73ef7dc8ce7090c0fc34ba07b
zvenigorodskaia <work1908@gmail.com>
2021-02-03 22:52:50 +0300

    solution for 02-02-MeetupPageComponent

--- 02-components/02-MeetupPageComponent/MeetupView.js
+++ 02-components/02-MeetupPageComponent/MeetupView.js
@@ -1,8 +1,8 @@
-// import { MeetupCover } from './MeetupCover.js';
-// import { MeetupDescription } from './MeetupDescription.js';
-// import { MeetupAgenda } from './MeetupAgenda.js';
-// import { MeetupInfo } from './MeetupInfo.js';
-// import { getMeetupCoverLink } from './data.js';
+import { MeetupCover } from './MeetupCover.js';
+import { MeetupDescription } from './MeetupDescription.js';
+import { MeetupAgenda } from './MeetupAgenda.js';
+import { MeetupInfo } from './MeetupInfo.js';
+import { getMeetupCoverLink } from './data.js';
 
 export const MeetupView = {
   name: 'MeetupView',
@@ -10,23 +10,43 @@ export const MeetupView = {
   template: `
     <div>
       <!-- meetup cover -->
+      <meetup-cover :title="meetup.title" :link="coverLink"></meetup-cover>
       <div class="container">
         <div class="meetup">
           <div class="meetup__content">
-            <h3>Описание</h3>
+            <meetup-description v-if="meetup.description" :description="meetup.description"></meetup-description>
             <!-- meetup description -->
 
-            <h3>Программа</h3>
+            <h3 v-if="meetup.agenda.length > 0">Программа</h3>
             <!-- meetup agenda -->
+            <meetup-agenda :agenda="meetup.agenda"></meetup-agenda>
           </div>
           <div class="meetup__aside">
+            <meetup-info :organizer="meetup.organizer" :place="meetup.place" :date="meetupDate"></meetup-info>
             <!-- meetup info -->
           </div>
         </div>
       </div>
     </div>`,
 
-  // components
-
-  // props
+  components: {
+    MeetupDescription,
+    MeetupAgenda,
+    MeetupCover,
+    MeetupInfo,
+  },
+  props: {
+    meetup: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    coverLink() {
+      return this.meetup.imageId ? getMeetupCoverLink(this.meetup) : null;
+    },
+    meetupDate() {
+      return this.meetup.date ? new Date(this.meetup.date) : null;
+    },
+  },
 };
